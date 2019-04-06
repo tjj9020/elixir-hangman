44942f372ef8dab65468ebfbb9f013bda6197f9c
Tim Johnson <timothy.johnson2@purina.nestle.com>
2019-04-06 09:49:51 -0400

    add state and testing, add guard

--- hangman/test/game_test.exs
+++ hangman/test/game_test.exs
@@ -9,6 +9,17 @@ defmodule GameTest do
     assert game.turns_left == 7
     assert game.game_state == :initializing
     assert length(game.letters) > 0
+
+    results = for l <- game.letters, do: l =~ ~r(^[^A-Z]*$)
+    assert results
+  end
+
+  test "state isnt changed for :won/:lost game" do
+    for state <- [ :won, :lost ] do
+      game = Game.new_game()
+        |> Map.put(:game_state, state)
+      assert { ^game, _ } = Game.make_move(game, "x")
+    end
   end
 
 end
