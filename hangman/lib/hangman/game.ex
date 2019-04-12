762d3ed6858b34a2c09de6afeead12db68633de8
Tim Johnson <timothy.johnson2@purina.nestle.com>
2019-04-12 08:20:27 -0400

    set private functions to defp

--- hangman/lib/hangman/game.ex
+++ hangman/lib/hangman/game.ex
@@ -26,31 +26,6 @@ defmodule Hangman.Game do
     { game, tally(game) }
   end
 
-  def accept_move(game, guess, _already_guessed = true) do
-    Map.put(game, :game_state, :already_used )
-  end
-
-  def accept_move(game, guess, _already_guessed) do
-    Map.put(game, :used, MapSet.put(game.used, guess))
-    |> score_guess(Enum.member?(game.letters, guess))
-  end
-
-  def score_guess(game, _good_guess = true) do 
-    new_state = MapSet.new(game.letters)
-    |> MapSet.subset?(game.used)
-    |> maybe_won()
-    Map.put(game, :game_state, new_state)
-  end
-
-  def score_guess(game = %{ turns_left: 1 }, _not_good_guess) do
-    %{ game | game_state: :lost }
-  end
-
-  def score_guess(game = %{ turns_left: turns_left }, _not_good_guess) do
-    %{ game | game_state: :bad_guess,
-       turns_left: turns_left - 1 }
-  end
-
   def tally(game) do
     %{
       game_state: game.game_state,
@@ -59,14 +34,44 @@ defmodule Hangman.Game do
     }
   end
 
+  ###################################################################
+
+
+  defp accept_move(game, guess, _already_guessed = true) do
+    Map.put(game, :game_state, :already_used )
+  end
+
+  defp accept_move(game, guess, _already_guessed) do
+    Map.put(game, :used, MapSet.put(game.used, guess))
+    |> score_guess(Enum.member?(game.letters, guess))
+  end
+
+  defp score_guess(game, _good_guess = true) do 
+    new_state = MapSet.new(game.letters)
+    |> MapSet.subset?(game.used)
+    |> maybe_won()
+    Map.put(game, :game_state, new_state)
+  end
+
+  defp score_guess(game = %{ turns_left: 1 }, _not_good_guess) do
+    %{ game | game_state: :lost }
+  end
+
+  defp score_guess(game = %{ turns_left: turns_left }, _not_good_guess) do
+    %{ game | game_state: :bad_guess,
+       turns_left: turns_left - 1 }
+  end
+
+  
+
   def reveal_guessed(letters, used) do
     letters
     |> Enum.map(fn letter -> reveal_letter(letter, MapSet.member?(used, letter)) end)
   end
 
-  def reveal_letter(letter, _in_word = true), do: letter
-  def reveal_letter(letter, _not_in_word), do: "_"
+  defp reveal_letter(letter, _in_word = true), do: letter
+  defp reveal_letter(letter, _not_in_word), do: "_"
 
-  def maybe_won(true), do: :won
-  def maybe_won(_), do: :good_guess
+  defp maybe_won(true), do: :won
+  defp maybe_won(_), do: :good_guess
 end
