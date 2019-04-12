a53d106a34963b9840dd713663c77fd0b5e10181
Tim Johnson <timothy.johnson2@purina.nestle.com>
2019-04-12 08:18:18 -0400

    add items to public api

--- hangman/lib/hangman.ex
+++ hangman/lib/hangman.ex
@@ -3,5 +3,6 @@ defmodule Hangman do
   alias Hangman.Game #will now refer to only the part after the period ""
 
   defdelegate new_game(), to: Game
-
+  defdelegate make_move(game, guess), to :Game
+  defdelegate tally(game), to: Game
 end
